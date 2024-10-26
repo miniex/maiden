afd447546dd2c84151607100f70510b024036a3d
Han Damin <miniex@daminstudio.net>
2024-10-26 15:35:07 +0900

    refactor: add internal

---
+++ crates/maiden_internal/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod cuda;
+pub mod prelude;
+
+pub use maiden_tensor as tensor;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,1 @@
-pub use maiden_tensor as tensor;
-
-pub mod prelude {
-    pub use maiden_tensor::Tensor;
-}
+pub use maiden_internal::*;
