de8f983f607f7f68f3d9c8acf5f3b21129b5a932
Han Damin <miniex@daminstudio.net>
2024-10-31 15:48:05 +0900

    chore: fix examples

--- examples/grad/grad-nonauto.rs
+++ examples/grad/grad-nonauto.rs
@@ -16,19 +16,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start = Instant::now();
 
     let learning_rate = 1e-6;
-    for t in 1..2000 {
+    for t in 1..=5000 {
         let y_pred = &a + &b * &x + &c * x.pow(2.0)? + &d * x.pow(3.0)?;
         let loss = (&y_pred - &y).pow(2.0)?.sum()?;
 
-        if t != 0 && t % 100 == 0 {
-            println!("t: {}, loss: {}", t, loss);
+        if t % 100 == 0 {
+            println!("t: {}, loss: {}", t, loss.item()?);
         }
 
         let grad_y_pred = 2.0 * (&y_pred - &y);
         let grad_a = grad_y_pred.sum()?;
         let grad_b = (&grad_y_pred * &x).sum()?;
-        let grad_c = (&grad_y_pred * &x.pow(2.0)?).sum()?;
-        let grad_d = (&grad_y_pred * &x.pow(3.0)?).sum()?;
+        let grad_c = (&grad_y_pred * x.pow(2.0)?).sum()?;
+        let grad_d = (&grad_y_pred * x.pow(3.0)?).sum()?;
 
         a = &a - learning_rate * grad_a;
         b = &b - learning_rate * grad_b;
